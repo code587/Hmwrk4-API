b7a913107796d2c4764c44cb91c8bea07512e69d
Stephanie <sandora375@outlook.com>
2021-11-16 01:42:38 -0600

    timer added to countdown

---
+++ script.js
@@ -0,0 +1,30 @@
+let startButton = document.querySelector(".startbtn");
+let timerCountdown = document.querySelector("#timer");
+let quizQuestions = document.querySelector(".questions");
+let quizAnswers = document.querySelector(".answers");
+let questionAnswer1 = [("Diamond", " Garnet", " Sapphire", " Emerald")];
+let timeLeft = 60;
+
+startButton.addEventListener("click", function(){
+   
+    let timerInterval = setInterval(function() {
+        if (timeLeft > 0) {
+            timerCountdown.textContent = timeLeft + " seconds";
+            timeLeft--;
+        } else if (timeLeft === 0) {
+            timerCountdown.textContent = "Game Over";
+            clearInterval(timeInterval);
+            displayMessage();
+        }
+    }, 1000);
+})
+    
+
+    //quizQuestions.textContent = ("What is the January birthstone?");
+    
+
+    
+
+   
+
+   
