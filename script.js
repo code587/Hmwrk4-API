7b5fe2e0ffcec065aa0603c1d0c3ff3fe3f9eaf9
Stephanie <sandora375@outlook.com>
2021-11-16 15:39:08 -0600

    corrected index spelling

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let startButton = document.querySelector(".startbtn");
+let quizInstructions = document.querySelector("#instructions")
 let timerCountdown = document.querySelector("#timer");
 let quizQuestions = document.querySelector(".questions");
 let quizAnswers = document.querySelector(".answers");
-let questionAnswer1 = [("Diamond", " Garnet", " Sapphire", " Emerald")];
+let questionAnswer1 = ["Diamond", " Garnet", " Sapphire", " Emerald"];
 let timeLeft = 60;
 
 startButton.addEventListener("click", function(){
-   
+    instructions.textContent = quizInstructions
+    console.log("instructions")
     let timerInterval = setInterval(function() {
         if (timeLeft > 0) {
             timerCountdown.textContent = timeLeft + " seconds";
@@ -17,11 +19,9 @@ startButton.addEventListener("click", function(){
             displayMessage();
         }
     }, 1000);
-})
+quizQuestions.textContent = "What is the January birthstone?";})
     
 
-    //quizQuestions.textContent = ("What is the January birthstone?");
-    
 
     
 
