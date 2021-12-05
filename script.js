74d8431e319b49e225bdd91bce73c7af3620f607
Stephanie <sandora375@outlook.com>
2021-12-05 16:18:57 -0600

    added questions one and two with answer buttons

--- script.js
+++ script.js
@@ -4,20 +4,27 @@ let quizQ1 = document.querySelector("#question1");
 let quizQ2 = document.querySelector("#question2");
 let quizQ3 = document.querySelector("#question3");
 let quizQ4 = document.querySelector("#question4");
-// let quizAnswers = document.querySelector(".answers");
-// let userAnswer = document.createElementbyclass("userResponse");
+let q1Opt1 = document.querySelector("quiz1option1");
+let q1Opt2 = document.querySelector("quiz1option2");
+let q1Opt3 = document.querySelector("quiz1option3");
+let q1Opt4 = document.querySelector("quiz1option4");
+let q2Answrs = document.querySelector("answersq2");
+let q3Answrs = document.querySelector("answersq3");
+let q4Answrs = document.querySelector("answersq4");
+
 let timerCountdown = document.querySelector("#timer");
+
 let questionsList = [
     quizQ1.textContent = "What type of car do the brothers drive in the show?",
+    
     quizQ2.textContent = "What character is named after an executive producer?",
-    quizQ3.textContent = "What is the theme song for the show Supernatural?",
-    quizQ4.textContent = "Which actor/actress met their spouse on the set?"
+    
 ]
 
 
-console.log(questionsList);
 
 startBtn.addEventListener("click", function() {
+    let questions = 0
     let timeLeft = 5
     let timerInterval = setInterval(function() {
         if (timeLeft > 1) { 
@@ -33,14 +40,15 @@ startBtn.addEventListener("click", function() {
             clearInterval(timerInterval);
         }
     }, 1000);
-    function questionsList() {
-        i[0];
-        for (let i = 0; i < questionsList.length; i++) {
-        console.log(questions.length);
-            
-        }
-    }
+
+    [0]
+    for (let i = 0; i < questionsList.length; i++) {
+        console.log(questionsList);
+    }    
+
+    
 })
+
     
 
 
