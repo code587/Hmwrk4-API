e6d11015c50df22d0f13bb72cf7b7ddd3f94aabd
Stephanie <sandora375@outlook.com>
2021-12-04 22:09:57 -0600

    added click event listener to time

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-// let startBtn = document.querySelector("#startbutton");
+let startBtn = document.querySelector("#startbutton");
 // let timerCountdown = document.querySelector("#timer");
 // let quizQuestions = document.querySelector(".questions");
 // let quizAnswers = document.querySelector(".answers");
 // let userAnswer = document.createElementbyclass("userResponse");
-let timerEl = document.querySelector(".time");
+let timerCountdown = document.querySelector("#timer");
 let timeLeft = 5;
 
 
 
-function setTime() {
+startBtn.addEventListener("click",function() {
     
     let timerInterval = setInterval(function() {
-        timeLeft--;
-        timerEl.textContent = timeLeft + " seconds";
-                    
+        timeLeft--; 
+        timerCountdown.textContent = timeLeft + " seconds";
+             
         if (timeLeft === 0) {
-            clearInterval(timerInterval);
-            sendMessage()
-;        }
-    }, 1000);
-    console.log(setTime);
-    console.log(timerInterval);
-}
+            clearInterval(timerInterval);  
+            sendMessage();
+        }
+            
+}, 1000);
+   
+})
+    
+    function sendMessage() {
+        timerCountdown.textContent = timeLeft + " Game Over. Do you want to see your score";
+    }
 
-function sendMessage() {
-    timerEl.textContent = "Game Over. Do you want to see your score? ";
-}
   
-  setTime();
 
 
 
