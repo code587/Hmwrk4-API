c06a5a27bfbbbc1106984bbd8f049b43a569f186
Stephanie <sandora375@outlook.com>
2021-12-05 01:47:46 -0600

    added quiz questions

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
 let startBtn = document.querySelector("#startbutton");
-// let timerCountdown = document.querySelector("#timer");
-// let quizQuestions = document.querySelector(".questions");
+let questions = document.querySelector(".quizquestions");
+let quizQ1 = document.querySelector("#question1");
+let quizQ2 = document.querySelector("#question2");
+let quizQ3 = document.querySelector("#question3");
+let quizQ4 = document.querySelector("#question4");
 // let quizAnswers = document.querySelector(".answers");
 // let userAnswer = document.createElementbyclass("userResponse");
 let timerCountdown = document.querySelector("#timer");
-let timeLeft = 5;
+let questionsList = [
+    quizQ1.textContent = "What type of car do the brothers drive in the show?",
+    quizQ2.textContent = "What character is named after an executive producer?",
+    quizQ3.textContent = "What is the theme song for the show Supernatural?",
+    quizQ4.textContent = "Which actor/actress met their spouse on the set?"
+]
 
 
+console.log(questionsList);
 
-startBtn.addEventListener("click",function() {
-    
+startBtn.addEventListener("click", function() {
+    let timeLeft = 5
     let timerInterval = setInterval(function() {
-        timeLeft--; 
+        if (timeLeft > 1) { 
         timerCountdown.textContent = timeLeft + " seconds";
-             
-        if (timeLeft === 0) {
-            clearInterval(timerInterval);  
-            sendMessage();
+     startBtn.hidden = true
+
+    timeLeft--;
+    } else if (timeLeft === 1) {
+        timerCountdown.textContent = timeLeft + " second left";
+    timeLeft--;
+    } else { 
+            timerCountdown.textContent = " Game Over";
+            clearInterval(timerInterval);
         }
+    }, 1000);
+    function questionsList() {
+        i[0];
+        for (let i = 0; i < questionsList.length; i++) {
+        console.log(questions.length);
             
-}, 1000);
-   
+        }
+    }
 })
     
-    function sendMessage() {
-        timerCountdown.textContent = timeLeft + " Game Over. Do you want to see your score";
-    }
 
-  
+
+
+
+
+
+    
 
 
 
