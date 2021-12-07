76c35afa4c5ff2e57029463a3f402b742c5cac1a
Stephanie <sandora375@outlook.com>
2021-12-07 15:05:20 -0600

    added full questions list with answers and stopped timer if all questions answered

--- script.js
+++ script.js
@@ -1,53 +1,118 @@
 let startBtn = document.querySelector("#startbutton");
-let questions = document.querySelector(".quizquestions");
-let quizQ1 = document.querySelector("#question1");
-let quizQ2 = document.querySelector("#question2");
-let quizQ3 = document.querySelector("#question3");
-let quizQ4 = document.querySelector("#question4");
-let q1Opt1 = document.querySelector("quiz1option1");
-let q1Opt2 = document.querySelector("quiz1option2");
-let q1Opt3 = document.querySelector("quiz1option3");
-let q1Opt4 = document.querySelector("quiz1option4");
-let q2Answrs = document.querySelector("answersq2");
-let q3Answrs = document.querySelector("answersq3");
-let q4Answrs = document.querySelector("answersq4");
-
+let questionsDiv = document.querySelector("#quizquestions");
+let answers = document.querySelector(".quizanswers");
 let timerCountdown = document.querySelector("#timer");
+let timeLeft = 10
 
-let questionsList = [
-    quizQ1.textContent = "What type of car do the brothers drive in the show?",
-    
-    quizQ2.textContent = "What character is named after an executive producer?",
-    
+
+const questions = [
+    {
+        title: "What type of car do the brothers drive in the show?",
+        choices: ["Impala", "Chevette", "Mustang", "Sabre"],
+        answer: "Impala"
+    },
+    {
+        title: "What character is named after an executive producer?",
+        choices: ["Dean", "Cass", "Mary", "Bobby"],
+        answer: "Bobby"
+    },
+    {
+        title: "What is the theme song for the show Supernatural?",
+        choices: ["Round and Round", "Wayward Son", "Back in Black", "Ratt"],
+        answer: "Wayward Son"
+    },
+    {
+        title: "Which actor/actress met their spouse on the set?",
+        choices:  ["Jenson Ackles", "Kim Rhodes", "Jared Padalecki", "Felicia Day"],
+        answer: "Jared Padalecki"
+    }
 ]
-
-
-
+//remove the hide attribute to show questions.  Timer begins and start button is hidden
 startBtn.addEventListener("click", function() {
+    questionsDiv.removeAttribute("class")
     let questions = 0
-    let timeLeft = 5
+  
     let timerInterval = setInterval(function() {
         if (timeLeft > 1) { 
         timerCountdown.textContent = timeLeft + " seconds";
      startBtn.hidden = true
-
     timeLeft--;
     } else if (timeLeft === 1) {
         timerCountdown.textContent = timeLeft + " second left";
     timeLeft--;
-    } else { 
-            timerCountdown.textContent = " Game Over";
+    } 
+    else { 
+            timerCountdown.textContent = "";
             clearInterval(timerInterval);
         }
     }, 1000);
 
-    [0]
-    for (let i = 0; i < questionsList.length; i++) {
-        console.log(questionsList);
-    }    
-
-    
+    displayQuestions()
 })
+
+let currentQuestionList = 0;
+let choicesEl = document.getElementById("questionChoices")
+
+function displayQuestions(){
+    let currentQuestion = questions[currentQuestionList] // questions[0]
+
+    // will display the questions[0] title
+    let titleEl = document.getElementById("questionTitle")
+    titleEl.textContent = currentQuestion.title
+
+    // display the choices as buttons
+
+    choicesEl.innerHTML = "";
+
+    currentQuestion.choices.forEach(function(choice, i){
+        let choiceBtn = document.createElement("button")
+        choiceBtn.setAttribute("value", choice)
+
+        choiceBtn.textContent = choice;
+
+        choicesEl.append(choiceBtn)
+
+        choiceBtn.onclick = handleQuestionClick;
+    })
+};
+// this is where it indicates if user chose the correct answer and to move on to the next question
+
+let correctScore = 0
+
+function handleQuestionClick(){
+if(this.value === questions[currentQuestionList].answer){
+        correctScore ++;
+          console.log("correctAnswer")
+    } else {
+        console.log("wrongAnswer")
+    }
+currentQuestionList++;
+
+    if(currentQuestionList === questions.length){
+        questionsDiv.setAttribute("class", "hide");
+        timeLeft = 0
+    console.log(correctScore);
+        quizScore(currentScore, questions.length)
+        
+
+        // fire off an end quiz function handling your score and whatnot
+     
+    } else {
+        displayQuestions();
+    }
+}
+
+function quizScore(correctAnswer, questions) {
+    let score = correctAnswer/ questions * 100
+    quizScore.textContent = "Your score is " + quizScore + "Do you want to play again?";
+    return score;
+}
+
+
+
+
+
+
 
     
 
